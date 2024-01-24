13b6dc97cf98f61b42515b10f38c1e8f5dd6b423
lee <viatoweb@sina.com>
2024-01-24 19:41:44 +0800

    wallet

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/viaonweb/wallet/main/public/tonconnect-manifest.json";
+  "https://img.war6sky.com/resources/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
